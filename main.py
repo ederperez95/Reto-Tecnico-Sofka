8eabc216548ad070d5be635997f5ad2e555850ef
USER <ederperezgarcia@gmail.com>
2021-12-28 01:59:24 -0500

    funcion de validacion de respuestas de usuario y respuestas correctas

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
 
 import funciones
 
-def menuPrincipal():
-    continuar = True
-    while(continuar):
-        opcionCorrecta = False
-        while(not opcionCorrecta):
-            print("============= Menu principal =============")
-            print("1. Crear Jugador")
-            print("2. Listar datos Jugador Completo")
-            print("3. Listar  Jugador sin contraseña")
-            print("4. Actualizar todo menos la puntuacion")
-            print("5. Actualizar la puntuacion")
-            print("6. Eliminar Jugador")
-            print("7. Salir Jugador")
-            print("============================================")
+#Se Leen y se clasifican las preguntas
+ListaDePreguntasClasificadasPorNivel = funciones.clasificarListaPreguntas()
 
-            opcion = int(input("Seleccione una opcion: "))
-        
-            if opcion < 1 or opcion > 7:
-                print("Opcion incorrecta, ingrese nuevamente")
-            elif opcion == 7:
-                continuar = False
-                print("Gracias por usar este sistema!!")
-                break
-            else:
-                preguntasCategoriaUno, preguntasCategoriaDos, preguntasCategoriaTres, preguntasCategoriaCuatro, preguntasCategoriaCinco = funciones.clasificarListaPreguntas()
+#funciones.menuPrincipal()
 
-                opcionCorrecta = True
-
-menuPrincipal()
+funciones.menuPrincipal(ListaDePreguntasClasificadasPorNivel)
