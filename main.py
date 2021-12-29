5670a83f883ff1be4b5242e93774e35297787205
USER <ederperezgarcia@gmail.com>
2021-12-29 02:08:05 -0500

    Agregar funcion a la opcion 2 del menu

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import Clases
 
 categoriasUsadas = ["Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5"]
 
+
 opcionCorrecta = False
 continuar = True
 Menu = Clases.Menu
 while(continuar):
     while(not opcionCorrecta):
+        #menu para seleccionar si crear jugador o uno ya existente
         Menu.menuInicial()
         opcion = int(input("Seleccione una opcion: "))
 
+        
         if opcion < 1 or opcion > 3:
             print("Opcion incorrecta, ingrese una opcion nuevamente")
 
-
+        #Opcion 1 Seleccionar jugador para posteriormente jugar con ese usuario
         elif opcion == 1:
 
             nuevoJugador = Clases.Jugador()
@@ -32,7 +35,7 @@ while(continuar):
                         continuar = False
                         break
                         
-
+            #Empieza el menu de juego nuevo 
             opcionCorrecta = False
             continuar = True
             while(continuar):
@@ -48,6 +51,7 @@ while(continuar):
                         cometioErrores = False
                         nuevoPremio = Clases.Premio()
                         premioAcumulado = 0
+                        #Desarrollo del juego. Las preguntas con sus verificaciones
                         for categoria in categoriasUsadas:
                             funciones.limpiarPantalla()
                             nuevaCategoria = Clases.Categoria()
@@ -83,9 +87,63 @@ while(continuar):
                         continuar = False
                         break   
 
+        #Opcion 2 creaar jugador para posteriormente jugar con ese usuario
         elif opcion == 2:
             nuevoJugador = Clases.Jugador()
             jugador = nuevoJugador.crearJugador(nombreArchivoJugadores = "jugadores.txt")
+            #Empieza el menu de juego nuevo 
+            opcionCorrecta = False
+            continuar = True
+            while(continuar):
+                while(not opcionCorrecta):
+                    
+                    funciones.limpiarPantalla()
+                    Menu.menuNuevoJuego()
+                    opcion = int(input("Seleccione una opcion: "))
+                    
+                    if opcion < 1 or opcion > 2:
+                        print("Opcion incorrecta, ingrese una opcion nuevamente")
+                    elif opcion == 1:
+                        cometioErrores = False
+                        nuevoPremio = Clases.Premio()
+                        premioAcumulado = 0
+                        #Desarrollo del juego. Las preguntas con sus verificaciones
+                        for categoria in categoriasUsadas:
+                            funciones.limpiarPantalla()
+                            nuevaCategoria = Clases.Categoria()
+                            nuevaCategoria.categoria("preguntas.txt", categoria)
+                            preguntasCategoria = nuevaCategoria.getListaPreguntasPorCategoria()
+                            nuevaPregunta  = Clases.Pregunta()
+                            nuevaPregunta.seleccionPreguntaAleatoria(preguntasCategoria)
+                            nuevaPregunta.mostrarPregunta()
+                            preguntaAleatoria = nuevaPregunta.getPreguntaAleatoria()
+                            respuestaJugador = int(input("Seleccione una opcion: "))
+                            if respuestaJugador == 5:
+                                print("Te has retirado de juego.")
+                                resultado = "Deserto"
+                                nuevoPremio.mostrarPremio(resultado, premioAcumulado)
+                                break
+                            nuevaValidacion = Clases.ValidacionRespuesta()
+                            nuevaValidacion.validacion(preguntaAleatoria, respuestaJugador)
+                            nuevaValidacion.mostrarResultado()
+                            resultado = nuevaValidacion.getResultado()
+                            nuevoPremio.premioCategoria(categoria, premioAcumulado)
+                            nuevoPremio.mostrarPremio(resultado, premioAcumulado)
+                            premioAcumulado = nuevoPremio.getPremio()
+                            
+                            if resultado == "Perdio":
+                                break
+                        if resultado == "Acerto":
+                            print("Felicidades al responder todas las preguntas correctamente de cada categoria has ganago el juego!!!")
+                            input("Presione cualquier tecla para continuar")
+
+                    elif opcion == 2:
+                        print("De regreso al menu anterior")
+                        input("Presione cualquier tecla para continuar")
+                        continuar = False
+                        break   
+
+        #Opcion 3 Salir del juego
         elif opcion == 3:
             print("Gracias por jugar!!")
             input("Presione cualquier tecla para continuar")
