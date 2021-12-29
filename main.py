4f59c92b0714b39987b9364df6ead29d8d5a8e2e
USER <ederperezgarcia@gmail.com>
2021-12-29 01:55:48 -0500

    Version con todas las funciones en el main

--- main.py
+++ main.py
@@ -1,35 +1,94 @@
 import funciones
 import Clases
 
+
+categoriasUsadas = ["Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5"]
+
 opcionCorrecta = False
 continuar = True
+Menu = Clases.Menu
 while(continuar):
     while(not opcionCorrecta):
-        Clases.Menu.menuInicial()
+        Menu.menuInicial()
         opcion = int(input("Seleccione una opcion: "))
 
         if opcion < 1 or opcion > 3:
             print("Opcion incorrecta, ingrese una opcion nuevamente")
+
+
         elif opcion == 1:
+
+            nuevoJugador = Clases.Jugador()
+            listaJugadores = nuevoJugador.listarJugadores(nombreArchivoJugadores = "jugadores.txt")
+            opcionCorrecta = False
+            continuar = True
+            while(continuar):
+                while(not opcionCorrecta):
+                    opcionJugadorSeleccionado = int(input("Seleccione un jugador para empezar el juego: "))
+                    if opcionJugadorSeleccionado < 1 or opcionJugadorSeleccionado > len(listaJugadores):
+                        print("Opcion incorrecta, ingrese una opcion nuevamente")
+                    else:
+                        jugadorSeleccionado = listaJugadores[opcionJugadorSeleccionado - 1]
+                        continuar = False
+                        break
+                        
 
             opcionCorrecta = False
             continuar = True
             while(continuar):
                 while(not opcionCorrecta):
-
+                    
                     funciones.limpiarPantalla()
-                    Clases.Menu.menuNuevoJuego()
+                    Menu.menuNuevoJuego()
                     opcion = int(input("Seleccione una opcion: "))
                     
-                    if opcion < 1 or opcion > 3:
+                    if opcion < 1 or opcion > 2:
                         print("Opcion incorrecta, ingrese una opcion nuevamente")
                     elif opcion == 1:
-                        
+                        cometioErrores = False
+                        nuevoPremio = Clases.Premio()
+                        premioAcumulado = 0
+                        for categoria in categoriasUsadas:
+                            funciones.limpiarPantalla()
+                            nuevaCategoria = Clases.Categoria()
+                            nuevaCategoria.categoria("preguntas.txt", categoria)
+                            preguntasCategoria = nuevaCategoria.getListaPreguntasPorCategoria()
+                            nuevaPregunta  = Clases.Pregunta()
+                            nuevaPregunta.seleccionPreguntaAleatoria(preguntasCategoria)
+                            nuevaPregunta.mostrarPregunta()
+                            preguntaAleatoria = nuevaPregunta.getPreguntaAleatoria()
+                            respuestaJugador = int(input("Seleccione una opcion: "))
+                            if respuestaJugador == 5:
+                                print("Te has retirado de juego.")
+                                resultado = "Deserto"
+                                nuevoPremio.mostrarPremio(resultado, premioAcumulado)
+                                break
+                            nuevaValidacion = Clases.ValidacionRespuesta()
+                            nuevaValidacion.validacion(preguntaAleatoria, respuestaJugador)
+                            nuevaValidacion.mostrarResultado()
+                            resultado = nuevaValidacion.getResultado()
+                            nuevoPremio.premioCategoria(categoria, premioAcumulado)
+                            nuevoPremio.mostrarPremio(resultado, premioAcumulado)
+                            premioAcumulado = nuevoPremio.getPremio()
+                            
+                            if resultado == "Perdio":
+                                break
+                        if resultado == "Acerto":
+                            print("Felicidades al responder todas las preguntas correctamente de cada categoria has ganago el juego!!!")
+                            input("Presione cualquier tecla para continuar")
+
+                    elif opcion == 2:
+                        print("De regreso al menu anterior")
+                        input("Presione cualquier tecla para continuar")
+                        continuar = False
+                        break   
 
         elif opcion == 2:
-            pass
+            nuevoJugador = Clases.Jugador()
+            jugador = nuevoJugador.crearJugador(nombreArchivoJugadores = "jugadores.txt")
         elif opcion == 3:
             print("Gracias por jugar!!")
+            input("Presione cualquier tecla para continuar")
             continuar = False
             break
 
